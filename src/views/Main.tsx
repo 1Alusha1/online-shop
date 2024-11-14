7c9d6e26b917a904b62a88d9e572247b16c39fc6
1alusha1 <alusha9636@gmail.com>
2024-11-14 20:16:57 +0200

    added infinit scroll to main page

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import data from "../../data.json";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -15,15 +16,57 @@ const Wrapper = styled.div`
   }
 `;
 
+interface Goods {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+  alt: string;
+}
+
+const PAGE_SIZE = 10;
+
+function generateUniqueId() {
+  return `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+}
+
 const Main = () => {
+  const [loadedGoods, setLoadedGoods] = useState<Goods[]>([]);
+  const [page, setPage] = useState(1);
+
+  const fetchGoods = (page: number) => {
+    const startIndex = (page - 1) * PAGE_SIZE;
+    const endIndex = page * PAGE_SIZE;
+
+    const newGoods: Goods[] = data.slice(startIndex, endIndex);
+
+    setLoadedGoods((prevGoods: Goods[]) => [...prevGoods, ...newGoods]);
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    fetchGoods(1);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight
+      )
+        return;
+      fetchGoods(page);
+    };
+    
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [page]);
+
   return (
     <div className="container">
       <Wrapper>
-        {data.map((good) => (
-          <Card key={good.id} good={good}></Card>
-        ))}
-        {data.map((good) => (
-          <Card key={good.id} good={good}></Card>
+        {loadedGoods.map((good) => (
+          <Card key={generateUniqueId()} good={good}></Card>
         ))}
       </Wrapper>
     </div>
